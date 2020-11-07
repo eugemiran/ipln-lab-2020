7da381e853aaa3e73dc9e4a9ddd2e2fa81147af0
Eugenia <eugemiran@gmail.com>
2020-11-07 17:14:23 -0300

    Modify es_odio

--- es_odio.py
+++ es_odio.py
@@ -1,7 +1,13 @@
-import utils
+from constants import MODEL_TYPES
+import data
+import model
 
 def main():
-    utils.test()
+    datasets = data.Data()
+    model_type = MODEL_TYPES["SIMPLE"] # HERE GOES THE MODEL WE CONFIGURED IN CORSS VALIDATION
+    m = model.Model(model_type=model_type, train_dataset=datasets.train, val_dataset=datasets.test)
+    m.train()
+    m.eval()
 
 if __name__ == "__main__":
     main()
